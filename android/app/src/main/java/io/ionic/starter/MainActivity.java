a78e2529140719e6f9e0afcfaed5bbcb0db1e466
lenafam96 <cong.minh.hvt@gmail.com>
2023-04-06 01:40:13 +0700

    dang ky FCM token tren ionic

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends BridgeActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create channel to show notifications.
@@ -55,24 +55,24 @@ public class MainActivity extends BridgeActivity  {
 
         askNotificationPermission();
 
-        FirebaseMessaging.getInstance().getToken()
-                .addOnCompleteListener(new OnCompleteListener<String>() {
-                    @Override
-                    public void onComplete(@NonNull Task<String> task) {
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
-                            return;
-                        }
-
-                        // Get new FCM registration token
-                        String token = task.getResult();
-
-                        // Log and toast
-                        String msg = getString(R.string.msg_token_fmt, token);
-                        Log.d(TAG, msg);
-                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-                    }
-                });
+//        FirebaseMessaging.getInstance().getToken()
+//                .addOnCompleteListener(new OnCompleteListener<String>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<String> task) {
+//                        if (!task.isSuccessful()) {
+//                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+//                            return;
+//                        }
+//
+//                        // Get new FCM registration token
+//                        String token = task.getResult();
+//
+//                        // Log and toast
+//                        String msg = getString(R.string.msg_token_fmt, token);
+//                        Log.d(TAG, msg);
+//                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//                    }
+//                });
     }
 
     private void askNotificationPermission() {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,73 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import { PushNotifications } from "@capacitor/push-notifications";
+import "./Home.css";
 
 const Home: React.FC = () => {
+  const addListeners = async () => {
+    await PushNotifications.addListener("registration", (token) => {
+      console.info("Đăng ký token: ", token.value);
+    });
+
+    await PushNotifications.addListener("registrationError", (err) => {
+      console.error("Registration error: ", err.error);
+    });
+
+    await PushNotifications.addListener(
+      "pushNotificationReceived",
+      (notification) => {
+        console.log("Push notification received: ", notification);
+      }
+    );
+
+    await PushNotifications.addListener(
+      "pushNotificationActionPerformed",
+      (notification) => {
+        console.log(
+          "Push notification action performed",
+          notification.actionId,
+          notification.inputValue
+        );
+      }
+    );
+  };
+
+  const registerNotifications = async () => {
+    let permStatus = await PushNotifications.checkPermissions();
+
+    if (permStatus.receive === "prompt") {
+      permStatus = await PushNotifications.requestPermissions();
+    }
+
+    if (permStatus.receive !== "granted") {
+      throw new Error("User denied permissions!");
+    }
+
+    await PushNotifications.register();
+  };
+
+  const getDeliveredNotifications = async () => {
+    const notificationList =
+      await PushNotifications.getDeliveredNotifications();
+    console.log("delivered notifications", notificationList);
+  };
+
+  const reg = async () => {
+    await registerNotifications();
+  };
+
+  const add = async () => {
+    await addListeners();
+  };
+
+  reg();
+  add();
   return (
     <IonPage>
       <IonHeader>
